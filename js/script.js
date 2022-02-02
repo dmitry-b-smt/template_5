df9b779d143dc51b3f18e546ef3e7142031d481f
unknown <peresvet.smt7101@gmail.com>
2022-02-02 23:05:07 +0300

    'works-section_sortie+adaptive'

--- js/script.js
+++ js/script.js
@@ -112,9 +112,9 @@ servicesColumns.forEach(function (item, i,) {
 const servicesItems = document.querySelectorAll('.services__item');
 servicesItems.forEach(function (item) {
     item.addEventListener('mouseenter', function (e) {
-        let column = item.closest('[data-serviceindex]');
-        let icon = column.querySelector('img');
-        let link = column.querySelector('.services__service-link');
+        let parent = item.closest('[data-serviceindex]');
+        let icon = parent.querySelector('img');
+        let link = parent.querySelector('.services__service-link');
         icon.style.opacity = 1;
         link.style.opacity = 1;
     })
@@ -122,10 +122,135 @@ servicesItems.forEach(function (item) {
 
 servicesItems.forEach(function (item) {
     item.addEventListener('mouseleave', function (e) {
-        let column = item.closest('[data-serviceindex]');
-        let icon = column.querySelector('img');
-        let link = column.querySelector('.services__service-link');
+        let parent = item.closest('[data-serviceindex]');
+        let icon = parent.querySelector('img');
+        let link = parent.querySelector('.services__service-link');
         icon.style.opacity = 0.25;
         link.style.opacity = 0.5;
+    })
+});
+
+
+
+// -------------------------WORKS_TABS----------------------------
+const allWorksButton = document.querySelector('.works__more');
+
+function setDataIndex(array) {
+    let counter = 0;
+    for (let item of array) {
+        var arrayIndex = counter;
+        if (!item.classList.contains('disabled')) {
+            item.setAttribute('data-arrayIndex', arrayIndex);
+            counter += 1;
+        } else {
+            counter += 0;
+        };
+    };
+};
+
+function setEvenOdd(array) {
+    array.forEach(function (item) {
+        var index = item.dataset.arrayindex;
+        if (parseInt(index) % 2 === 0) {
+            item.classList.add('works__card-even');
+        } else {
+            item.classList.add('works__card-odd');
+        };
+    })
+};
+
+function removeEvenOdd(array) {
+    array.forEach(function (item) {
+        item.classList.remove('works__card-even');
+        item.classList.remove('works__card-odd');
+    })
+}
+
+function removeDataIndex(array) {
+    array.forEach(function (item, i) {
+        let arrayIndex = i;
+        item.removeAttribute('data-arrayIndex', arrayIndex);
+    })
+}
+
+function hideMore(array) {
+    array.forEach(function (item) {
+        if (Number(item.dataset.arrayindex) > 3) {
+            item.classList.add('disabled');
+            allWorksButton.classList.remove('works__more-disabled');
+        } else {
+            allWorksButton.classList.add('works__more-disabled');
+        };
+    });
+}
+
+
+const worksTabsContainer = document.querySelector('.works__tabs');
+const worksTabs = document.querySelectorAll('.works__tab');
+const worksCards = document.querySelectorAll('.card');
+
+
+setDataIndex(worksTabs);
+
+
+worksTabs.forEach(function (item) {
+    item.addEventListener('click', function (e) {
+        let currentTab = worksTabsContainer.querySelector('.works__tab-active');
+        currentTab.classList.remove('works__tab-active');
+        item.classList.add('works__tab-active');
+    });
+});
+
+
+setDataIndex(worksCards);
+setEvenOdd(worksCards);
+hideMore(worksCards)
+
+
+worksTabsContainer.addEventListener('click', function (e) {
+    let target = e.target;
+    removeDataIndex(worksCards);
+    removeEvenOdd(worksCards);
+    if (target.classList.contains('works__tab')) {
+        worksCards.forEach(function (item) {
+            if (target.dataset.cathegory === 'all' || target.dataset.cathegory === item.dataset.cathegory) {
+                item.classList.remove('disabled');
+            } else {
+                item.classList.add('disabled');
+            };
+        });
+    };
+    setDataIndex(worksCards);
+    setEvenOdd(worksCards);
+    hideMore(worksCards);
+});
+
+
+allWorksButton.addEventListener('click', function (e) {
+    allWorksButton.classList.add('works__more-disabled');
+    worksCards.forEach(function (item) {
+        if (Number(item.dataset.arrayindex) > 3) {
+            item.classList.remove('disabled');
+        };
+    });
+})
+
+worksCards.forEach(function (item) {
+    item.addEventListener('mouseenter', function (e) {
+        let parent = item.closest('[data-arrayindex]');
+        let title = parent.querySelector('.card__title');
+        let link = parent.querySelector('.card__link');
+        title.classList.add('card__title-active');
+        link.classList.add('card__link-active');
+    })
+});
+
+worksCards.forEach(function (item) {
+    item.addEventListener('mouseleave', function (e) {
+        let parent = item.closest('[data-arrayindex]');
+        let title = parent.querySelector('.card__title');
+        let link = parent.querySelector('.card__link');
+        title.classList.remove('card__title-active');
+        link.classList.remove('card__link-active');
     })
 });
